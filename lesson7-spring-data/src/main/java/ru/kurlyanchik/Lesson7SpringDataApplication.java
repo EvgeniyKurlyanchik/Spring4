2df38fcd133a3c586181a272292a2249b195083a
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-08-31 23:00:05 +0300

    Задание 7

---
+++ lesson7-spring-data/src/main/java/ru/kurlyanchik/Lesson7SpringDataApplication.java
@@ -0,0 +1,13 @@
+package ru.kurlyanchik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson7SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson7SpringDataApplication.class, args);
+    }
+
+}
